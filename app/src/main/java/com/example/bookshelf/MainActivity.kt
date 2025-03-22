14145301aa7383e9217cbcc3f119ab7c2f394ded
Алексей <mr-kaifat@yandex.ru>
2025-03-22 12:41:36 +0300

    Внесены изменения в BooksScreen(LazyVerticalGrid)

--- app/src/main/java/com/example/bookshelf/MainActivity.kt
+++ app/src/main/java/com/example/bookshelf/MainActivity.kt
@@ -9,6 +9,16 @@ import com.example.bookshelf.ui.theme.screens.BooksScreen
 import com.example.bookshelf.viewmodel.BooksViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
+/*Это приложение для Android, которое использует Jetpack Compose
+для создания пользовательского интерфейса и Hilt для внедрения зависимостей.
+Оно позволяет пользователю искать книги через Google Books API и отображает результаты в виде списка
+Приложение запускается с BookshelfApplication, который инициализирует Hilt.
+MainActivity устанавливает содержимое экрана и создает BooksViewModel с помощью Hilt.
+BooksScreen отображает список книг, используя данные из BooksViewModel.
+BooksViewModel взаимодействует с BooksRepository, чтобы получить данные из API.
+BooksRepository использует GoogleBooksApiService для выполнения запросов к API.
+Данные, возвращаемые API, преобразуются в объекты Kotlin с помощью Retrofit и Gson.
+Результаты отображаются на экране с помощью Jetpack Compose. */
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/example/bookshelf/ui/theme/screens/BooksScreen.kt
+++ app/src/main/java/com/example/bookshelf/ui/theme/screens/BooksScreen.kt
@@ -1,9 +1,14 @@
 package com.example.bookshelf.ui.theme.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -14,13 +19,22 @@ import coil.compose.AsyncImage
 import com.example.bookshelf.data.BookItem
 import com.example.bookshelf.viewmodel.BooksViewModel
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+
 
 @Composable
 fun BooksScreen(viewModel: BooksViewModel) {
     val books by viewModel.books.collectAsState()
 
-    LazyColumn {
-        items(books) { book ->
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(books.size) { index ->
+            val book = books[index]
             BookItem(book)
         }
     }
@@ -34,12 +48,23 @@ fun BooksScreen(viewModel: BooksViewModel) {
 fun BookItem(book: BookItem) {
     val imageUrl = book.volumeInfo.imageLinks?.thumbnail?.replace("http://", "https://")
 
-    Column {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .aspectRatio(0.75f)
+            .padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         AsyncImage(
             model = imageUrl,
             contentDescription = null,
-            modifier = Modifier.size(100.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(0.66f) // Соотношение сторон изображения (например, 2:3)
         )
-        Text(text = book.volumeInfo.title)
+        Text(
+            text = book.volumeInfo.title,
+            modifier = Modifier.padding(top = 8.dp)
+        )
     }
 }
